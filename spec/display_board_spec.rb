5d6eab23ee70d4e08bc5547ecc77bfb0194efd9a
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-14 21:32:36 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -169,6 +169,7 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Os' do
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] 
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
